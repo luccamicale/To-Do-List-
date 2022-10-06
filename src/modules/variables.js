b40395cdb8a55522bbdc9a60d4088e97cdb43c53
luccamicale <luccamicale2015@gmail.com>
2022-10-06 17:16:28 -0300

    initial commit

--- src/modules/variables.js
+++ src/modules/variables.js
@@ -1,32 +1,4 @@
-const ulList = document.getElementById('toDo');
-const lists = [
-  {
-    description: 'Go to the supermarket',
-    completed: true,
-    index: 0,
-  },
-  {
-    description: 'Practice Sport',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Go to the park with my dog',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'Have a lunch',
-    completed: true,
-    index: 3,
-  },
-];
-export default class displayLists {
-  static addList() {
-    for (let i = 0; i < lists.length; i += 1) {
-      const list = document.createElement('li');
-      list.innerHTML = `<input type="checkbox" name="completed" id="completed"> ${lists[i].description} <i class="fa-solid fa-ellipsis-vertical"></i>`;
-      ulList.appendChild(list);
-    }
-  }
-}
+
+export const form = document.querySelector("#new-task-form");
+export const input = document.querySelector("#new-task-input");
+export const list_el = document.querySelector("#tasks");
